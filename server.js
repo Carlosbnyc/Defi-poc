0f28054cb800f1be5d48560171cc7f74f30b2803
Carlos <bowyercarlos@gmail.com>
2025-03-06 11:48:54 -0500

    updated dynamic site

--- server.js
+++ server.js
@@ -4,28 +4,26 @@ require('dotenv').config();
 
 const app = express();
 
-// Connect Database
-// connectDB();
-
-// Init Middleware
+// Middleware to parse JSON
 app.use(express.json());
 
-// Define Routes
+// Serve static files from public/
+app.use(express.static('public'));
+
+// Define API Routes
 app.use('/api/users', require('./server/routes/api/users'));
 app.use('/api/auth', require('./server/routes/api/auth'));
 app.use('/api/profile', require('./server/routes/api/profile'));
 app.use('/api/posts', require('./server/routes/api/posts'));
 
-// Serve static assets in production
+// Serve React frontend in production
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
   app.use(express.static('client/build'));
-
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
+// Start server
 const PORT = process.env.PORT || 5025;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
